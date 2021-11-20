f96d6af30a5ba4f87e803195294b115dcdc66850
okitauehara <marcosokita@live.com>
2021-11-19 23:59:01 -0300

    Fixing some query indentations

--- src/controllers/signatures.js
+++ src/controllers/signatures.js
@@ -33,19 +33,36 @@ export default async function postSignature(req, res) {
     const userId = getUser.rows[0].id;
 
     products.forEach(async (product) => {
-      await connection.query('INSERT INTO users_products (user_id, product_id) VALUES ($1, $2)', [userId, Number(product)]);
+      await connection.query(`
+      INSERT INTO users_products
+        (user_id, product_id)
+      VALUES
+        ($1, $2)`, [userId, Number(product)]);
     });
 
     const getDate = await connection.query('SELECT * FROM dates WHERE date = $1', [delivery_date]);
     const dateId = getDate.rows[0].id;
 
-    await connection.query('INSERT INTO delivery_infos (cep, number, date_id, full_name) VALUES ($1, $2, $3, $4)', [cep, number, dateId, full_name]);
+    await connection.query(`
+    INSERT INTO delivery_infos
+      (cep, number, date_id, full_name)
+    VALUES
+      ($1, $2, $3, $4)`, [cep, number, dateId, full_name]);
 
-    const getDeliveryInfos = await connection.query('SELECT * FROM delivery_infos WHERE cep = $1 AND number = $2 AND date_id = $3 AND full_name = $4', [cep, number, dateId, full_name]);
+    const getDeliveryInfos = await connection.query(`
+    SELECT * FROM delivery_infos
+      WHERE cep = $1
+        AND number = $2
+        AND date_id = $3
+        AND full_name = $4`, [cep, number, dateId, full_name]);
     const deliveryInfosId = getDeliveryInfos.rows[0].id;
 
     await connection.query('UPDATE users SET plan_id = $1 WHERE id = $2', [planId, userId]);
-    await connection.query('INSERT INTO signatures (user_id, plan_id, delivery_id) VALUES ($1, $2, $3)', [userId, planId, deliveryInfosId]);
+    await connection.query(`
+    INSERT INTO signatures
+      (user_id, plan_id, delivery_id)
+    VALUES
+      ($1, $2, $3)`, [userId, planId, deliveryInfosId]);
 
     return res.sendStatus(200);
   } catch {
