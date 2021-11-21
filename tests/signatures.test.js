f0c44812333400b3e00a457d5220c037b773c6ce
okitauehara <marcosokita@live.com>
2021-11-21 14:36:54 -0300

    Tests for getSignatures route implemented

--- tests/signatures.test.js
+++ tests/signatures.test.js
@@ -41,3 +41,23 @@ describe('POST /subscriptions/:planId', () => {
     expect(result.status).toEqual(404);
   });
 });
+
+describe('GET /subscriptions', () => {
+  test('should return status 401 if the request was missing token', async () => {
+    const result = await supertest(app).get('/subscriptions/');
+    expect(result.status).toEqual(401);
+  });
+
+  test('should return status 200 if the request was successfull', async () => {
+    const result = await supertest(app).get('/subscriptions/').set('Authorization', `Bearer ${U.fakeSession.token}`);
+    expect(result.status).toEqual(200);
+    expect(result.body).toHaveProperty('signature_date');
+    expect(result.body).toHaveProperty('products');
+    expect(result.body).toHaveProperty('plan');
+  });
+
+  test('should return status 404 if the request token does not belong to any user', async () => {
+    const result = await supertest(app).get('/subscriptions/').set('Authorization', `Bearer ${U.fakeSession.token}123`);
+    expect(result.status).toEqual(404);
+  });
+});
